4bdefb87511dade6d314c0745f0860c7c64b2ca3
jacob <jacobcunningham96@yahoo.com>
2020-07-18 16:47:18 -0600

    updated color

--- src/components/Flavor.js
+++ src/components/Flavor.js
@@ -4,13 +4,15 @@ const Flavor = (props) => {
   const [tally, setTally] = useState(0);
 
   const onVote = () => setTally(tally + 1);
+
+  const voted = tally > 0 ? "#0AA129" : "#1BCDFB";
   return (
     <>
       <tr>
         <td data-label="flavor">{props.flavor}</td>
         <td data-label="tally">{tally}</td>
         <td onClick={() => onVote()} data-label="plus">
-          <i className="plus icon"></i>
+          <i style={{ color: voted }} className="plus icon"></i>
         </td>
       </tr>
     </>
